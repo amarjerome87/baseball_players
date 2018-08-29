421a94eb442bf60368aae6c039ac2376c8a23cd3
Amar Chatman <amarjerome@gmail.com>
2018-08-29 06:58:23 +0000

    Add list_players method

--- lib/baseball_players/cli.rb
+++ lib/baseball_players/cli.rb
@@ -2,5 +2,14 @@ class BaseballPlayers::CLI
   
   def call 
     puts "This is a list of great baseball players!"
+    list_players 
   end 
+  
+  def list_players
+    puts <<-DOC 1.player_name
+                2.player_name
+           DOC 
+  end 
+  
+  
 end 
