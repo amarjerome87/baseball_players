79dc530a632c84dd247c2fb84a6bc14e0772b2ad
Amar Chatman <amarjerome@gmail.com>
2018-08-29 07:18:15 +0000

    Add while and case statements

--- lib/baseball_players/cli.rb
+++ lib/baseball_players/cli.rb
@@ -1,18 +1,29 @@
 class BaseballPlayers::CLI
   
   def call 
-    puts "This is a list of great baseball players!"
     list_players
     choices 
   end 
   
   def list_players
+    puts "This is a list of great baseball players!"
     puts  "1.player_name"
     puts  "2.player_name"
             
   end 
   
   def choices 
+    puts "Enetr the number of the player you would like to learn more about or type list to see the player list again or exit to enter"
+    input = nil 
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "more info on choosen baseball player 1"
+      when "2"
+        puts "more info on choosen baseball player 2"
+      end
+    end 
   end 
   
   
