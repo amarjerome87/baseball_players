161fa362c0db66c9be6e4565fabb2f2e65bf1175
Amar Chatman <amarjerome@gmail.com>
2018-09-11 16:17:19 +0000

    create new class method

--- lib/baseball_players.rb
+++ lib/baseball_players.rb
@@ -1,4 +1,3 @@
 require_relative "./baseball_players/version"
 require_relative "./baseball_players/cli"
 require_relative "./baseball_players/player"
-require 'nokogiri'
--- lib/baseball_players/player.rb
+++ lib/baseball_players/player.rb
@@ -4,8 +4,15 @@ class BaseballPlayers::Player
 
   def self.rank
     # return players with rank with associated player
+    self.scrape_players
 
-    doc = Nokogiri::HTML(open("https://www.britannica.com/list/10-greatest-baseball-players-of-all-time"))
+  end
+
+    # doc = Nokogiri::HTML(open("https://www.britannica.com/list/10-greatest-baseball-players-of-all-time"))
+  def self.scrape_players
+
+    players = []
+
 
 
     player_1 = self.new
@@ -13,47 +20,10 @@ class BaseballPlayers::Player
     player_1.ranking = "10"
     player_1.summary = "stuff"
 
-    # player_2 = player.new
-    # player_2.name = "x"
-    # player_2.ranking = "9"
-    # player_2.summary = "stuff"
-    #
-    # player_3 = player.new
-    # player_3.name = "y"
-    # player_3.ranking = "7"
-    # player_3.summary = "stuff"
-    #
-    # player_4 = player.new
-    # player_4.name = "r"
-    # player_4.ranking = "6"
-    # player_4.summary = "stuff"
-    #
-    # player_5 = player.new
-    # player_5.name = "y"
-    # player_5.ranking = "5"
-    # player_5.summary = "stuff"
-    #
-    # player_6 = player.new
-    # player_6.name = "i"
-    # player_6.ranking = "4"
-    # player_6.summary = "stuff"
-    #
-    # player_7 = player.new
-    # player_7.name = "f"
-    # player_7.ranking = "3"
-    # player_7.summary = "stuff"
-    #
-    # player_8 = player.new
-    # player_8.name = "dsf"
-    # player_8.ranking = "2"
-    # player_8.summary = "stuff"
-    #
-    # player_9 = player.new
-    # player_9.name = "hjk"
-    # player_9.ranking = "1"
-    # player_9.summary = "stuff"
 
     [player_1]
+
+    deals 
 
   end
 
