30434fd527e21ecfc2dbaa75698096d0bc6cf111
Amar Chatman <amarjerome@gmail.com>
2018-09-13 18:43:08 +0000

    add list_doc

--- lib/baseball_players/player.rb
+++ lib/baseball_players/player.rb
@@ -10,14 +10,16 @@ class BaseballPlayers::Player
 
   def self.scrape_players
 
-    players = []
-
     doc = Nokogiri::HTML(open("https://www.britannica.com/list/10-greatest-baseball-players-of-all-time"))
-
-    @player = self.new
-    @player.name = doc.css("h2").text.scan(/[A-Z][a-z]+/)
-    @player.summary = doc.css("p").text
-    players <<  @player
+    list_doc = doc.css("ul") #container ul
+    binding.pry
+    list_doc.collect.with_index do|li,i|
+      binding.pry
+      player = self.new
+      player.name = doc.css("h2")[i].text.scan(/[A-Z][a-z]+/)
+      player.summary = doc.css("p").text
+      player
+    end
   end
 
 end
