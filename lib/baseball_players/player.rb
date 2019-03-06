158379f569c4da0bd1d54dc39762309775830e3d
Amar Chatman <amarjerome@gmail.com>
2019-03-06 20:41:10 +0000

    added @@all variable and self.all method

--- lib/baseball_players/player.rb
+++ lib/baseball_players/player.rb
@@ -1,11 +1,21 @@
 class BaseballPlayers::Player
 
-  attr_accessor :name, :summary
+  attr_accessor :name, :summary, :player
+  
+  @@all = []
 
+  def initialize(player)
+    @player = player
+    @@all << self
+  end
 
-#define @@all
-#need initialize method
-#store player instances in @@all
-#need self.all method so the cli can access the players 
+  def self.all
+    @@all
+  end
+
+  #define @@all
+  #need initialize method
+  #store player instances in @@all
+  #need self.all method so the cli can access the players
 
 end
